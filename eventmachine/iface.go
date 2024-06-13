7b9dece6f353767747e457b895e7fa19ae6b5cfe
vikas-avnish <vikas.avnish@thecarousell.com>
2024-06-13 22:47:45 +0530

    event machine

---
+++ eventmachine/iface.go
@@ -0,0 +1,24 @@
+package eventmachine
+
+type EventPipeIfaces interface {
+	Push(event string, args ...interface{})
+	Pull() (event string, args []interface{})
+}
+
+type WhenIface interface {
+	When(event string, handler func(...interface{}))
+}
+type EventMachine interface {
+	WhenIface
+}
+
+type EventPipe struct {
+}
+
+func NewEventPipe() *EventPipe {
+	return &EventPipe{}
+}
+
+func (ep *EventPipe) Push(event string, args ...interface{}) {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ivikasavnish/req-multiplexor
+
+go 1.22.2
